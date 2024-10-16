448cb415d1ecd530f7ff7ad09da4e9f43db51a66
Varun Sadasivam <sadavar05@gmail.com>
2024-10-16 19:05:35 -0400

    basic mongo setup

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
+from dotenv import load_dotenv
 from routes.api import api_routes
 from routes.home import home_routes
-from flask import Flask
+from flask import Flask, g
 
 from flask_cors import CORS
+from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 
@@ -15,6 +18,30 @@ CORS(app)
 # Register the routes
 home_routes(app)
 api_routes(app)
+
+
+# load env
+load_dotenv()
+
+# Set up the MongoDB client
+mongo_uri = os.environ.get('MONGO_URI')
+client = MongoClient(mongo_uri)
+
+try:
+    # Set up the MongoDB client
+    mongo_uri = os.getenv('MONGO_URI')
+    print(mongo_uri)
+    client = MongoClient(mongo_uri)
+    db = client['claws']
+    print("MongoDB connection successful")
+except:
+    print("MongoDB connection failed")
+
+
+@app.before_request
+def before_request():
+    g.db = db
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
--- backend/routes/api.py
+++ backend/routes/api.py
@@ -1,4 +1,5 @@
-from flask import jsonify
+from flask import jsonify, g
+from bson.json_util import dumps
 
 
 def api_routes(app):
@@ -6,3 +7,14 @@ def api_routes(app):
     @app.route('/api')
     def api_home():
         return jsonify("API")
+
+    @app.route('/api/tasklist/', methods=['GET'])
+    def tasklist():
+        print("GET /api/tasklist/")
+        db = g.db
+        tasklist = db.tasklist
+
+        tasks = tasklist.find()
+        tasks = list(tasks)
+
+        return jsonify(dumps(tasks))
