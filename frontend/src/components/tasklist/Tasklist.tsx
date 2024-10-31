0239bb846374b817b664bab72b806c5e1725c556
oscjac <oscjac3@gmail.com>
2024-10-30 20:05:53 -0400

    Added initial code for Tasklist.tsx

---
+++ frontend/src/components/tasklist/Tasklist.tsx
@@ -0,0 +1,12 @@
+type TaskListProps = {
+    // TODO: List props
+    prop: string
+}
+
+const Tasklist = ({ prop }: TaskListProps) => {
+    return <div>
+        {prop}
+    </div>
+}
+
+export default Tasklist
