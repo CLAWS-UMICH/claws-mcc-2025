b5f11e9fc4ba95886fece37ce75f4cc1e323ef8e
Emma Cherrin <echerrin@umich.edu>
2024-10-31 18:10:12 -0400

    ADDED: Screen Share Search bar. Changed theme to dark and removed factory flex box settings causing everything to be vertically aligned in the center of the page.

--- frontend/src/pages/SendScreens.tsx
+++ frontend/src/pages/SendScreens.tsx
@@ -1,15 +1,30 @@
-//import { useState, useEffect } from 'react'
-import './SendScreens.css'
+import { useState } from 'react';
+import './SendScreens.css';
 
 function SendScreens() {
+  const [query, setQuery] = useState("");
+
+  const search = (e: React.FormEvent) => {
+    e.preventDefault(); // Prevent page reload on form submission
+    alert("Clicked search with: " + query);
+  }
 
   return (
-    <>
-      <div>
-        Emma Rocks
+    <div id="container">
+      <div id="search-bar">
+        <h4 id="screen-share-title">Screen Share</h4>
+        <form onSubmit={search} id="search-input-form">
+          <input 
+            id="search-input"
+            type="text" 
+            onChange={(e) => setQuery(e.target.value)} 
+            placeholder="  Search Images" 
+            value={query}
+          />
+        </form>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default SendScreens
+export default SendScreens;
