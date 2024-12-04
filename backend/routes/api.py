e6444c247fe23dee3760634171ee1dfcd5db528a
oscjac <oscjac3@gmail.com>
2024-12-04 18:50:52 -0500

    Implemented tasklist API endpoints

--- backend/routes/api.py
+++ backend/routes/api.py
@@ -2,27 +2,27 @@ from flask import Flask, jsonify, request, g
 from bson.json_util import dumps
 
 
-def api_routes(app):
-
+def api_routes(app: Flask):
     @app.route('/api')
     def api_home():
         return jsonify("API")
 
-    @app.route('/api/tasklist/', methods=['GET'])
+    @app.route('/api/tasklist/', methods=['GET', 'POST'])
     def tasklist():
-        print("GET /api/tasklist/")
-        db = g.db
-        tasklist = db.tasklist
-
-        tasks = tasklist.find()
-        tasks = list(tasks)
-
-        return jsonify(dumps(tasks))
+        if request.method == 'POST':
+            app.logger.info("Adding task")
+            task = request.json
+            g.db.tasklist.insert_one(task)
+            return jsonify(success=True)
+        else:  # GET
+            app.logger.info("Getting tasks")
+            tasks = g.db.tasklist.find()
+            return dumps(tasks)
 
     @app.route('/api/number', methods=['GET', 'POST'])
     def manage_number():
         db = g.db
-        demo_data = db.demoData 
+        demo_data = db.demoData
 
         if request.method == 'GET':
             # Retrieve the first document in the collection
@@ -39,5 +39,5 @@ def api_routes(app):
 
             # Update or insert the document with the new number
             demo_data.update_one({}, {"$set": {"number": new_number}}, upsert=True)
-            
+
             return jsonify(success=True, number=new_number)
