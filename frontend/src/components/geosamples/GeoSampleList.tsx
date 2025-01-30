62aed4f3bab9d5b774327874655944ca393cab94
oscjac <oscjac3@gmail.com>
2025-01-29 20:25:02 -0500

    Changed stuff

--- frontend/src/components/geosamples/GeoSampleList.tsx
+++ frontend/src/components/geosamples/GeoSampleList.tsx
@@ -1,6 +1,6 @@
 // DropdownMenu.tsx
-import React from 'react';
-import { 
+import React from "react";
+import {
     Accordion,
     AccordionHeader,
     AccordionItem,
@@ -9,16 +9,19 @@ import {
     Skeleton,
     CompoundButton,
     makeStyles
-} from '@fluentui/react-components';
-import { ChevronUp16Regular, ChevronDown16Regular, Gesture20Filled } from '@fluentui/react-icons';
-import { BaseGeosample, BaseZone, ManagerAction } from './GeosampleTypes.tsx';
-import './Geosamples.css'
-import StarredSample from './StarredImage.tsx';
-import SampleImage from './SampleImage.tsx';
+} from "@fluentui/react-components";
+import {
+    ChevronUp16Regular,
+    ChevronDown16Regular
+} from "@fluentui/react-icons";
+import { BaseGeosample, BaseZone, ManagerAction } from "./GeosampleTypes.tsx";
+import "./Geosamples.css";
+import StarredSample from "./StarredImage.tsx";
+import SampleImage from "./SampleImage.tsx";
 
 const useStyles = makeStyles({
     container: {
-        marginLeft: ".2rem",
+        marginLeft: ".2rem"
     },
     header: {
         marginLeft: "-15px",
@@ -36,16 +39,16 @@ const useStyles = makeStyles({
         marginLeft: "32.5px"
     },
     sampleContainer: {
-      display: "flex",
-      alignItems: "left",
-      cursor: "pointer",
+        display: "flex",
+        alignItems: "left",
+        cursor: "pointer"
     },
     imageText: {
         fontSize: "12.5px",
         position: "absolute",
         marginTop: "-29.5px",
         marginLeft: "6px"
-    },
+    }
 });
 
 interface SampleListProps {
@@ -54,33 +57,42 @@ interface SampleListProps {
     dispatch: React.Dispatch<ManagerAction>;
     ready: boolean;
     selected?: BaseGeosample;
-};
+}
 
-const GeosampleList: React.FC<SampleListProps> = ({geosamples, sample_zones, dispatch, ready, selected}) => {
+const GeosampleList: React.FC<SampleListProps> = ({
+    geosamples,
+    sample_zones,
+    dispatch,
+    ready,
+    selected
+}) => {
     const styles = useStyles();
     const [openItems, setOpenItems] = React.useState<string[]>([]);
 
     const geosampleMap = React.useMemo(() => {
         const map = new Map();
-        geosamples.forEach(sample => map.set(sample.geosample_id, sample));
+        geosamples.forEach((sample) => map.set(sample.geosample_id, sample));
         return map;
     }, [geosamples]);
 
     React.useEffect(() => {
         if (sample_zones) {
-            setOpenItems(sample_zones.map(zone => zone.zone_id));
+            setOpenItems(sample_zones.map((zone) => zone.zone_id));
         }
     }, [sample_zones]);
 
-    const handleToggle: AccordionToggleEventHandler<string> = (event, data) => {
+    const handleToggle: AccordionToggleEventHandler<string> = (_, data) => {
         setOpenItems(data.openItems);
     };
 
-    const handleSelect = (dispatch: React.Dispatch<ManagerAction>, geosample?: BaseGeosample) => {
+    const handleSelect = (
+        dispatch: React.Dispatch<ManagerAction>,
+        geosample?: BaseGeosample
+    ) => {
         if (geosample && selected?.geosample_id === geosample.geosample_id) {
-            dispatch({type: 'deselect'});
+            dispatch({ type: "deselect" });
         } else if (geosample) {
-            dispatch({type: 'select', payload: geosample});
+            dispatch({ type: "select", payload: geosample });
         }
     };
 
@@ -89,158 +101,90 @@ const GeosampleList: React.FC<SampleListProps> = ({geosamples, sample_zones, dis
     }
 
     return (
-        <Accordion className={styles.container} defaultOpenItems={sample_zones.map(zone => zone.zone_id)} onToggle={handleToggle} openItems={openItems} multiple collapsible>
-            {sample_zones.map((zone) =>             
-                zone.ZoneGeosamplesIds.length > 0 && (
-                    <AccordionItem key={zone.zone_id} value={zone.zone_id}>
-                        <AccordionHeader className={styles.header} size="large" expandIcon={openItems.includes(zone.zone_id) ? <ChevronDown16Regular /> : <ChevronUp16Regular />} expandIconPosition="end">
-                            <b style={{fontSize:"15px"}}>Zone {String.fromCharCode(Number(zone.zone_id))}</b>
-                        </AccordionHeader>
-                        {zone.ZoneGeosamplesIds.map((id) => (
-                            <AccordionPanel className={styles.sampleContainer} key={id}>
-                                {geosampleMap.has(Number(id)) && (
-                                    <CompoundButton
-                                        style={{fontSize: "13px", width: "210px", height: "45px", border: "0px", background: "#0F0F0F"}}
-                                        onClick={() => handleSelect(dispatch, geosampleMap.get(Number(id)))}
-                                        shape='circular'
-                                        secondaryContent={geosampleMap.get(Number(id)).rock_type}
-                                        icon={!geosampleMap.get(Number(id)).starred ? <SampleImage sample={geosampleMap.get(Number(id))}/> : <StarredSample sample={geosampleMap.get(Number(id))}/>}
-                                    >
-                                        {geosampleMap.get(Number(id)).eva_data.name}
-                                    </CompoundButton>
-                                )}
-                            </AccordionPanel>
-                        ))}
-                    </AccordionItem>
-                )
+        <Accordion
+            className={styles.container}
+            defaultOpenItems={sample_zones.map((zone) => zone.zone_id)}
+            onToggle={handleToggle}
+            openItems={openItems}
+            multiple
+            collapsible
+        >
+            {sample_zones.map(
+                (zone) =>
+                    zone.ZoneGeosamplesIds.length > 0 && (
+                        <AccordionItem key={zone.zone_id} value={zone.zone_id}>
+                            <AccordionHeader
+                                className={styles.header}
+                                size="large"
+                                expandIcon={
+                                    openItems.includes(zone.zone_id) ? (
+                                        <ChevronDown16Regular />
+                                    ) : (
+                                        <ChevronUp16Regular />
+                                    )
+                                }
+                                expandIconPosition="end"
+                            >
+                                <b style={{ fontSize: "15px" }}>
+                                    Zone{" "}
+                                    {String.fromCharCode(Number(zone.zone_id))}
+                                </b>
+                            </AccordionHeader>
+                            {zone.ZoneGeosamplesIds.map((id) => (
+                                <AccordionPanel
+                                    className={styles.sampleContainer}
+                                    key={id}
+                                >
+                                    {geosampleMap.has(Number(id)) && (
+                                        <CompoundButton
+                                            style={{
+                                                fontSize: "13px",
+                                                width: "210px",
+                                                height: "45px",
+                                                border: "0px",
+                                                background: "#0F0F0F"
+                                            }}
+                                            onClick={() =>
+                                                handleSelect(
+                                                    dispatch,
+                                                    geosampleMap.get(Number(id))
+                                                )
+                                            }
+                                            shape="circular"
+                                            secondaryContent={
+                                                geosampleMap.get(Number(id))
+                                                    .rock_type
+                                            }
+                                            icon={
+                                                !geosampleMap.get(Number(id))
+                                                    .starred ? (
+                                                    <SampleImage
+                                                        sample={geosampleMap.get(
+                                                            Number(id)
+                                                        )}
+                                                    />
+                                                ) : (
+                                                    <StarredSample
+                                                        sample={geosampleMap.get(
+                                                            Number(id)
+                                                        )}
+                                                    />
+                                                )
+                                            }
+                                        >
+                                            {
+                                                geosampleMap.get(Number(id))
+                                                    .eva_data.name
+                                            }
+                                        </CompoundButton>
+                                    )}
+                                </AccordionPanel>
+                            ))}
+                        </AccordionItem>
+                    )
             )}
         </Accordion>
     );
-}
+};
 
 export default GeosampleList;
-
-// import React from 'react';
-// import { 
-//     Accordion,
-//     AccordionHeader,
-//     AccordionItem,
-//     AccordionPanel,
-//     AccordionToggleEventHandler,
-//     Skeleton,
-//     CompoundButton,
-//     makeStyles
-// } from '@fluentui/react-components';
-// import { ChevronUp16Regular, ChevronDown16Regular, Gesture20Filled } from '@fluentui/react-icons';
-// import { BaseGeosample, BaseZone, ManagerAction } from './GeosampleTypes.tsx';
-// import './Geosamples.css'
-// import StarredSample from './StarredImage.tsx';
-// import SampleImage from './SampleImage.tsx';
-
-// const useStyles = makeStyles({
-//     container: {
-//         marginLeft: ".2rem",
-//     },
-//     header: {
-//         marginLeft: "-15px",
-//         marginRight: "-15px"
-//     },
-//     sample: {
-//         fontSize: "14px",
-//         marginTop: ".25rem",
-//         marginBottom: "0px"
-//     },
-//     type: {
-//         fontSize: "12.25px",
-//         marginTop: "-20px",
-//         marginBottom: "17.5px",
-//         marginLeft: "32.5px"
-//     },
-//     sampleContainer: {
-//       display: "flex",
-//       alignItems: "left",
-//       cursor: "pointer",
-//     },
-//     imageText: {
-//         fontSize: "12.5px",
-//         position: "absolute",
-//         marginTop: "-29.5px",
-//         marginLeft: "6px"
-//     },
-// });
-
-// interface SampleListProps {
-//     geosamples: BaseGeosample[];
-//     sample_zones: BaseZone[];
-//     dispatch: React.Dispatch<ManagerAction>;
-//     ready: boolean;
-//     selected?: BaseGeosample;
-// };
-
-// const GeosampleList: React.FC<SampleListProps> = ({geosamples, sample_zones, dispatch, ready, selected}) => {
-//     const styles = useStyles();
-//     const [openItems, setOpenItems] = React.useState<string[]>([]);
-
-//     const geosampleMap = React.useMemo(() => {
-//         const map = new Map();
-//         if (geosamples && geosamples?.length) {
-//             geosamples.forEach(sample => map.set(sample.geosample_id, sample));
-//         }
-//         return map;
-//     }, [geosamples]);
-
-//     React.useEffect(() => {
-//         if (sample_zones) {
-//             setOpenItems(sample_zones.map(zone => zone.zone_id));
-//         }
-//     }, [sample_zones]);
-
-//     // Maintain state of accordion panels
-//     const handleToggle: AccordionToggleEventHandler<string> = (event, data) => {
-//         setOpenItems(data.openItems);
-//     };
-
-//     // Handle selecting sample from list & displaying corresponding detail screen
-//     const handleSelect = (dispatch: React.Dispatch<ManagerAction>, geosample?: BaseGeosample) => {
-//         if (geosample && selected?.geosample_id === geosample.geosample_id) { // change to id after finishing with front & back end
-//             dispatch({type: 'deselect'});
-//         }
-//         else if (geosample) {
-//             dispatch({type: 'select', payload: geosample});
-//         }
-//     };
-
-//     // If no zones are available or page is not ready, return empty list
-//     if (!ready || !sample_zones || sample_zones.length === 0 || !geosamples || geosamples.length === 0) {
-//         return <Skeleton />
-//     };
-
-//     return (
-//         <Accordion className={styles.container} defaultOpenItems={sample_zones.map(zone => zone.zone_id)} onToggle={handleToggle} openItems={openItems} multiple collapsible>
-//             {sample_zones.map((zone) =>             
-//                 zone.ZoneGeosamplesIds.length > 0 && (<AccordionItem key={zone.zone_id} value={zone.zone_id}>
-//                     <AccordionHeader key={zone.zone_id} className={styles.header} size="large" expandIcon={openItems.includes(zone.zone_id) ? <ChevronDown16Regular /> : <ChevronUp16Regular />} expandIconPosition="end">
-//                         <b style={{fontSize:"15px"}}>Zone {String.fromCharCode(Number(zone.zone_id))}</b>
-//                     </AccordionHeader>
-//                     {zone.ZoneGeosamplesIds.map((id, index) => (
-//                         <AccordionPanel style={{marginLeft: "-15px", marginRight: "-12px", marginBottom: "1px"}} className={styles.sampleContainer} key={id}>
-//                             {geosampleMap.has(Number(id)) && (
-//                                 <CompoundButton
-//                                     style={{fontSize: "13px", width: "210px", height: "45px", border: "0px", background: "#0F0F0F"}}
-//                                     onClick={() => handleSelect(dispatch, geosampleMap.get(Number(id)))}
-//                                     shape='circular'
-//                                     secondaryContent={geosampleMap.get(Number(id)).rock_type}
-//                                     icon={!geosampleMap.get(Number(id)).starred ? <SampleImage sample={geosampleMap.get(Number(id))}/> : <StarredSample sample={geosampleMap.get(Number(id))}/>}
-//                                 >
-//                                     {geosampleMap.get(Number(id)).eva_data.name}
-//                                 </CompoundButton>
-//                             )}
-//                         </AccordionPanel>
-//                     ))}
-//                 </AccordionItem>)
-//             )}
-//         </Accordion>
-//     );
-// }
-
-// export default GeosampleList;
--- frontend/src/components/geosamples/Geosamples.tsx
+++ frontend/src/components/geosamples/Geosamples.tsx
@@ -1,6 +1,5 @@
 // Geosamples.tsx
-import React, { useEffect, useReducer, useState } from 'react';
-import { ReadyState } from 'react-use-websocket';
+import React, { useReducer, useState } from "react";
 import {
     Button,
     Divider,
@@ -9,61 +8,64 @@ import {
     DrawerHeaderTitle,
     InlineDrawer,
     makeStyles,
-    shorthands,
+    shorthands
 } from "@fluentui/react-components";
 import { Search20Regular } from "@fluentui/react-icons";
-import SearchComponent from '../common/SearchBox/SearchBox.tsx'
-import DetailScreen from './DetailScreen.tsx';
-import GeosampleList from './GeosampleList.tsx';
-import GeosampleMap from './GeosampleMap.tsx';
-import useDynamicWebSocket from '../../hooks/useWebSocket.tsx';
-import { ManagerAction, ManagerState } from './GeosampleTypes.tsx';
+import DetailScreen from "./DetailScreen.tsx";
+import GeosampleMap from "./GeosampleMap.tsx";
+import GeosampleList from "./GeoSampleList.tsx";
+import { ManagerAction, ManagerState } from "./GeosampleTypes.tsx";
 
 const useStyles = makeStyles({
-  root: {
-    ...shorthands.overflow("hidden"),
-    display: "flex",
-    height: "100vh",
-    backgroundColor: "#000",
-  },
-  dividerContainer: {
-    width: "100%",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    ...shorthands.gap("10px"),
-    backgroundColor: "#0F0F0F"
-  },
-  header: {
-    paddingTop: "11px",
-    paddingBottom: "11px",
-
-  },
+    root: {
+        ...shorthands.overflow("hidden"),
+        display: "flex",
+        height: "100vh",
+        backgroundColor: "#000"
+    },
+    dividerContainer: {
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        ...shorthands.gap("10px"),
+        backgroundColor: "#0F0F0F"
+    },
+    header: {
+        paddingTop: "11px",
+        paddingBottom: "11px"
+    }
 });
 
-export const geosampleReducer = (state: ManagerState, action: ManagerAction): ManagerState => {
+export const geosampleReducer = (
+    state: ManagerState,
+    action: ManagerAction
+): ManagerState => {
     switch (action.type) {
-        case 'set':
+        case "set":
             return {
                 ...state,
                 geosamples: action.payload.samples,
-                sample_zones: action.payload.zones,
+                sample_zones: action.payload.zones
             };
-        case 'delete':
+        case "delete":
             return {
                 ...state,
                 geosamples: state.geosamples.filter(
-                    geosample => geosample.geosample_id !== action.payload.geosample_id
+                    (geosample) =>
+                        geosample.geosample_id !== action.payload.geosample_id
                 ),
-                sample_zones: state.sample_zones.map(zone => ({
+                sample_zones: state.sample_zones.map((zone) => ({
                     ...zone,
-                    geosample_ids: zone.ZoneGeosamplesIds.filter(id => id !== action.payload.geosample_id)
-                })),
+                    geosample_ids: zone.ZoneGeosamplesIds.filter(
+                        (id) => id !== action.payload.geosample_id
+                    )
+                }))
             };
-        case 'update':
+        case "update":
             if (action.payload === undefined) {
                 return state;
-            };
+            }
 
             return {
                 ...state,
@@ -73,22 +75,22 @@ export const geosampleReducer = (state: ManagerState, action: ManagerAction): Ma
                     }
                     return sample;
                 }),
-                sample_zones: state.sample_zones.map(zone => ({
+                sample_zones: state.sample_zones.map((zone) => ({
                     ...zone,
-                    geosample_ids: zone.ZoneGeosamplesIds.map(id => {
+                    geosample_ids: zone.ZoneGeosamplesIds.map((id) => {
                         if (id === action.payload.geosample_id) {
                             return action.payload.geosample_id;
                         }
                         return id;
                     })
-                })),
+                }))
             };
-        case 'select':
+        case "select":
             return {
                 ...state,
                 selected: action.payload
             };
-        case 'deselect':
+        case "deselect":
             return {
                 ...state,
                 selected: undefined
@@ -98,58 +100,82 @@ export const geosampleReducer = (state: ManagerState, action: ManagerAction): Ma
     }
 };
 
-const initialState: ManagerState = {geosamples: [], sample_zones: [], selected: undefined};
+const initialState: ManagerState = {
+    geosamples: [],
+    sample_zones: [],
+    selected: undefined
+};
 
 // TODO :( -> get astronaut location to add icon to geosample list & map
-const GeosampleManager: React.FC = () => { 
-    const styles = useStyles();   
+const GeosampleManager: React.FC = () => {
+    const styles = useStyles();
     const [state, dispatch] = useReducer(geosampleReducer, initialState);
-    const [messageHistory, setMessageHistory] = useState<string[]>([]);
     const [showSearchBar, setShowSearchBar] = useState(false);
-
-    const {sendMessage, lastMessage, readyState} = useDynamicWebSocket({
-        onOpen: () => sendMessage(JSON.stringify({type: 'GET_SAMPLES', platform: 'FRONTEND'})),
-        type: 'SEND_SAMPLES'
-    });
-
-    useEffect(() => {
-        if (lastMessage !== null) {
-            setMessageHistory((prev) => prev.concat(lastMessage.data));
-            dispatch({type: 'set', payload: JSON.parse(lastMessage.data).data});
-            // console.log(JSON.parse(lastMessage.data).data)
-        }
-    }, [lastMessage, setMessageHistory]);
-
-    // TODO: implement search feature
-    const handleSearch = () => {
-        // state.sample_zones = state.sample_zones.map(zone => ({
-        //     ...zone,
-        //     geosample_ids: zone.geosample_ids.filter(id => id !== action.payload.geosample_id)
-        // }))
-    }
 
     return (
         <div className={styles.root}>
-            <InlineDrawer style={{width:"220px", background: "#141414", borderTopColor: "rgb(82, 82, 82)", borderRightColor: "rgb(82, 82, 82)"}}separator open>
+            <InlineDrawer
+                style={{
+                    width: "220px",
+                    background: "#141414",
+                    borderTopColor: "rgb(82, 82, 82)",
+                    borderRightColor: "rgb(82, 82, 82)"
+                }}
+                separator
+                open
+            >
                 <DrawerHeader className={styles.header}>
-                    <DrawerHeaderTitle action={<Button size="medium" icon={<Search20Regular/>} onClick={() => setShowSearchBar(!showSearchBar)}></Button>}>
-                        <b style={{fontSize:"18px"}}>Samples</b>
+                    <DrawerHeaderTitle
+                        action={
+                            <Button
+                                size="medium"
+                                icon={<Search20Regular />}
+                                onClick={() => setShowSearchBar(!showSearchBar)}
+                            ></Button>
+                        }
+                    >
+                        <b style={{ fontSize: "18px" }}>Samples</b>
                     </DrawerHeaderTitle>
                 </DrawerHeader>
                 <div className={styles.dividerContainer}>
                     <Divider></Divider>
-                    {showSearchBar && (<div style={{flexGrow: "1"}}>               
-                                            <SearchComponent handleDismiss={() => setShowSearchBar(!showSearchBar)} handleSearch={handleSearch}/>
-                                            <Divider></Divider>
-                                        </div>)}
+                    {showSearchBar && (
+                        <div style={{ flexGrow: "1" }}>
+                            <Divider></Divider>
+                        </div>
+                    )}
                 </div>
-                <DrawerBody style={{background: "#0F0F0F"}}>
-                    <GeosampleList geosamples={state.geosamples} sample_zones={state.sample_zones} selected={state.selected} dispatch={dispatch} ready={readyState === ReadyState.OPEN}/>
+                <DrawerBody style={{ background: "#0F0F0F" }}>
+                    <GeosampleList
+                        geosamples={state.geosamples}
+                        sample_zones={state.sample_zones}
+                        selected={state.selected}
+                        dispatch={dispatch}
+                        ready={true}
+                    />
                 </DrawerBody>
             </InlineDrawer>
-            <DrawerBody style={{paddingLeft: '0px', paddingRight: '0px', background: "black"}}>
-                <DetailScreen geosample={state.selected} dispatch={dispatch} ready={readyState === ReadyState.OPEN}/>
-                {state.selected && state.geosamples && state.sample_zones && <GeosampleMap geosamples={state.geosamples} zones={state.sample_zones} dispatch={dispatch} ready={readyState === ReadyState.OPEN} selected={state.selected}/>}
+            <DrawerBody
+                style={{
+                    paddingLeft: "0px",
+                    paddingRight: "0px",
+                    background: "black"
+                }}
+            >
+                <DetailScreen
+                    geosample={state.selected}
+                    dispatch={dispatch}
+                    ready={true}
+                />
+                {state.selected && state.geosamples && state.sample_zones && (
+                    <GeosampleMap
+                        geosamples={state.geosamples}
+                        zones={state.sample_zones}
+                        dispatch={dispatch}
+                        selected={state.selected}
+                        ready={true}
+                    />
+                )}
             </DrawerBody>
         </div>
     );
